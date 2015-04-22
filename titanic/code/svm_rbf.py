17e22c96ebb9c73351ae4605e539f3dbb8496289
Ryan Bahneman <rbahneman@apple.com>
2015-04-22 02:25:58 -0700

    Normalizing the data.

--- titanic/code/svm_rbf.py
+++ titanic/code/svm_rbf.py
@@ -15,6 +15,16 @@ import pylab as P
 from sklearn import svm
 
 
+def normalize(data, mins=None, maxs=None):
+    if type(mins) == type(None):
+        mins = data.min(axis=0)
+    if type(maxs ) == type(None):
+        maxs = data.max(axis=0)
+
+    data = ((data - mins) * (2.0 / (maxs - mins))) - 1.0
+
+    return (data, mins, maxs)
+
 # Data cleanup
 #   Convert all strings to integer classifiers.
 #   Fill in the missing values of the data and make it complete.
@@ -66,26 +76,27 @@ def clean_data(data):
     return data.values
 
 # TRAIN DATA
-train_df = pd.read_csv('../data/train.csv', header=0)        # Load the train file into a dataframe
-
+train_df = pd.read_csv('../data/train.csv', header=0) 
+#Clean and normailze the data (don't normalize the first colum)
 train_data = clean_data(train_df)
-
+norm_train_data, norm_mins, norm_maxs = normalize(train_data[:,1:])
 
 # TEST DATA
-test_df = pd.read_csv('../data/test.csv', header=0)        # Load the test file into a dataframe
+test_df = pd.read_csv('../data/test.csv', header=0)
 
 # Collect the test data's PassengerIds before dropping it
 ids = test_df['PassengerId'].values
 
+# Clean and normalize the data
 test_data = clean_data(test_df)
-
+norm_test_data = normalize(test_data, norm_mins, norm_maxs)[0]
 
 print 'Training...'
 classifier = svm.SVC(kernel='rbf')
-classifier = classifier.fit( train_data[0::,1::], train_data[0::,0] )
+classifier = classifier.fit( norm_train_data, train_data[0::,0] )
 
 print 'Predicting...'
-output = classifier.predict(test_data).astype(int)
+output = classifier.predict(norm_test_data).astype(int)
 
 
 prediction_filepath = "../prediction/svm_rbf.csv"
