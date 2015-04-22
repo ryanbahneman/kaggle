28a1d0bdf10328d002881c3cc2261aa5bc79ed89
Ryan Bahneman <rbahneman@apple.com>
2015-04-22 01:33:34 -0700

    Increasing the number of trees and improving the median age estimates.

--- titanic/code/first_attempt.py
+++ titanic/code/first_attempt.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import csv as csv
 import os as os
+import pylab as P
 from sklearn.ensemble import RandomForestClassifier
 
 
@@ -28,19 +29,26 @@ def clean_data(data):
     # All missing Embarked -> just make them embark from most common place
     if len(data.Embarked[ data.Embarked.isnull() ]) > 0:
         most_common_port = data.Embarked.dropna().mode().values[0]
-        data['Embarked_%s' % most_common_port][data.Embarked.isnull()] = 1
+        data.loc[data.Embarked.isnull(), 'Embarked_%s' % most_common_port] = 1
 
-    # All the ages with no data -> make the median of all Ages
-    median_age = data['Age'].dropna().median()
-    if len(data.Age[ data.Age.isnull() ]) > 0:
-        data.loc[ (data.Age.isnull()), 'Age'] = median_age
+    # All the ages with no data -> make the median of all Ages in that ticket class
+    ticket_classes = np.unique(data.Pclass.values)
+    median_ages = dict()
+    for ticket_class in ticket_classes:
+        median_for_class = data[data.Pclass == ticket_class].dropna().Age.median()
+        median_ages[ticket_class] = median_for_class
+
+    for ticket_class in ticket_classes:
+        if len(data.Age[ data.Age.isnull() & (data.Pclass == ticket_class)]) > 0:
+            data.loc[ (data.Age.isnull() & (data.Pclass == ticket_class)), 'Age'] \
+                    = median_ages[ticket_class]
 
     # All the missing Fares -> assume median of their respective class
     if len(data.Fare[ data.Fare.isnull() ]) > 0:
         median_fare = np.zeros(3)
-        for f in range(0,3):                                              # loop 0 to 2
+        for f in range(0,3): # loop 0 to 2
             median_fare[f] = data[ data.Pclass == f+1 ]['Fare'].dropna().median()
-        for f in range(0,3):                                              # loop 0 to 2
+        for f in range(0,3): # loop 0 to 2
             data.loc[ (data.Fare.isnull()) & (data.Pclass == f+1 ), 'Fare'] = median_fare[f]
 
     # Remove the non-numeric colums
@@ -64,8 +72,9 @@ test_data = clean_data(test_df)
 
 
 print 'Training...'
-forest = RandomForestClassifier(n_estimators=100)
+forest = RandomForestClassifier(n_estimators=10000)
 forest = forest.fit( train_data[0::,1::], train_data[0::,0] )
+
 
 print 'Predicting...'
 output = forest.predict(test_data).astype(int)
