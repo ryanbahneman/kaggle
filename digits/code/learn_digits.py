5b7fb3c2401832080a68c45966f5716742ce6f35
Ryan Bahneman <rbahneman@apple.com>
2015-04-25 21:57:31 -0700

    Plotting an image

--- digits/code/learn_digits.py
+++ digits/code/learn_digits.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import numpy as np
 import IPython as ipy
-import PIL
+from PIL import Image
 
 
 training_data_path = "../data/train.csv"
@@ -10,6 +11,12 @@ training_data_path = "../data/train.csv"
 train_df = pd.read_csv(training_data_path, header=0)
 
 images_data = train_df.drop(["label"], axis=1).values
+
+
+# Plot an image
+a = images_data[1].reshape((28,28)).astype(np.uint8)
+img = Image.fromarray(a, mode="L")
+img.show()
 
 ipy.embed()
 
