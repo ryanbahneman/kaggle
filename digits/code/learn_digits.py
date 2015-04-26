5fb34e0e4fd96ab1835815f348cd37ffb442feff
Ryan Bahneman <rbahneman@apple.com>
2015-04-25 21:03:52 -0700

    Adding the beginning of the digits processor

---
+++ digits/code/learn_digits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import IPython as ipy
+import PIL
+
+
+training_data_path = "../data/train.csv"
+
+train_df = pd.read_csv(training_data_path, header=0)
+
+images_data = train_df.drop(["label"], axis=1).values
+
+ipy.embed()
+
+
